6d4165d7e80b9d996082653820d15f30164c3da5
YasserYka <yasseryka@gmail.com>
2021-02-02 18:55:03 +0300

    fix issue in hackerernews plugin

--- dashboard-plugins/hackernews.js
+++ dashboard-plugins/hackernews.js
@@ -1,4 +1,5 @@
-const got = require('got');
+// It's critical to declare imports with 'var'
+var got = require('got');
 
 module.exports = class Hackernews {
   
@@ -14,12 +15,6 @@ module.exports = class Hackernews {
             
             <div class="card-body">
                 <ul class="list-group list-group-flush" id="hackernews">
-                    {
-                        this.getStories().then(stories => stories.map(story =>
-                            <a target="_blank" href={story.url} class="list-group-item list-group-item-action"> {story.title} </a>
-                            )
-                        )
-                    }
                 </ul>
             </div>
             
@@ -35,29 +30,36 @@ module.exports = class Hackernews {
   
     async script() {
 
-        setInterval(function renderNews() {
-
-            document.getElementById("hackernews").innerHTML = this.getStories().then(stories => 
-                stories.map(story =>
-                    <a target="_blank" href={story.url} class="list-group-item list-group-item-action"> {story.title} </a>
-                )
-            );
-
-            // return it self to execute the function the first time script gets executed first time without waiting the first delay
-            return renderNews();
-        }, 3600000 /*  one hour in milliseconds */); 
+        this.renderNews();
+        
+        setInterval(this.renderNews, 3600000 /*  one hour in milliseconds */); 
     }
 
-    getStories(){
-        return this.request('https://hacker-news.firebaseio.com/v0/beststories.json?orderBy="$key"&limitToFirst=6').then(ids => 
-            Promise.all(ids.map(id => this.request(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)(id))).then(stories => {
+    renderNews() {
+        this.getStories().then(stories => 
+            document.getElementById("hackernews").innerHTML = stories.map(story =>
+                <a target="_blank" href={story.url} class="list-group-item list-group-item-action"> {story.title} </a>
+            ).join(' ')
+        );
+    }
+
+    async getStories() {
+
+        return await this.request('https://hacker-news.firebaseio.com/v0/beststories.json?orderBy="$key"&limitToFirst=6').then(ids => 
+            Promise.all(ids.map(id => this.getStory(id))).then(stories => {
+                
                 return stories;
             })
         );
     }
 
+    getStory(id) {
+
+        return this.request(`https://hacker-news.firebaseio.com/v0/item/${id}.json`);
+    }
+
     async request(url){
-        return got(url).then(response => {
+        return await got(url).then(response => {
         
             return JSON.parse(response.body);
         }).catch(error => {
