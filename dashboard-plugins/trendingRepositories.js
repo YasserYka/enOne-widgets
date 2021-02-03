a86b523dccb2f4631b10b7b0cdd6980f327a7bcd
YasserYka <yasseryka@gmail.com>
2021-02-03 21:53:30 +0300

    finished trending repo plugin

--- dashboard-plugins/trendingRepositories.js
+++ dashboard-plugins/trendingRepositories.js
@@ -1,54 +1,67 @@
 // It's critical to declare imports with 'var'
-var got = require('got');
-const cheerio = require('cheerio');
+var got = require("got");
+const cheerio = require("cheerio");
 
 module.exports = class TrendingRepositories {
-  
-    async initialize() { }
-  
-    async render() {
+  async initialize() {}
 
-      return (
-        <div class="card mt-2 shadow bg-dark text-center">
-        </div>
-      );
-    }
-  
-    async script() {
-
-        this.renderNews();
-        
-        setInterval(this.renderRepositories, 43200000 /* 12 hours in miliseconds */); 
-    }
-
-    renderRepositories() {
-        this.fetchTrendingPage().then(repositories => {
+  async render() {
+    return (
+      <div class="card">
+        <div class="card-body">
+          <div id="repositories-list" class="list-group">
             
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  async script() {
+    this.renderRepositories();
+
+    setInterval(this.renderRepositories, 43200000 /* 12 hours in miliseconds */);
+  }
+
+  renderRepositories() {
+    this.fetchTrendingPage().then((repositories) => {
+
+        document.getElementById('repositories-list').innerHTML = repositories.map(repository => {
+
+            return <a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">
+              <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">{repository.title}</h5>
+                <small>{repository.stars}</small>
+              </div>
+              <p class="mb-1">{repository.description}</p>
+              <small><a href={repository.url}>Open</a></small>
+            </a>
         });
-    }
+    });
+  }
 
-    async fetchTrendingPage(){
-        return await got('https://github.com/trending').then(response => {
-        
-            const selector = cheerio.load(response.body);
+  async fetchTrendingPage() {
+    return await got("https://github.com/trending")
+      .then((response) => {
+        const selector = cheerio.load(response.body);
 
-            return selector('.Box-row').get().map(element => {
-                element = cheerio(element);
-                
-                let uri = element.find("h1:nth-child(2) > a:nth-child(1)").attr('href').substring(1);
-        
-                return {
-                    'stars': element.find("span:contains('stars today')").text().trim(),
-                    'description': element.find("p:nth-child(3)").text().trim(),
-                    'repository': uri,
-                    'url': 'https://github.com/' + uri 
-                };
-            });
+        return selector(".Box-row")
+          .get()
+          .map((element) => {
+            element = cheerio(element);
 
-        }).catch(error => {
+            let uri = element.find("h1:nth-child(2) > a:nth-child(1)").attr("href").substring(1);
 
-            console.error(error);
-        });
-    }
-
-  };
+            return {
+              stars: element.find("span:contains('stars today')").text().trim(),
+              description: element.find("p:nth-child(3)").text().trim(),
+              title: uri,
+              url: "https://github.com/" + uri,
+            };
+          });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+};
