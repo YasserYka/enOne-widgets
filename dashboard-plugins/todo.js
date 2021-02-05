9b20f30b4dfb23b24dfd7b06bee6e80a6d6b0153
YasserYka <yasseryka@gmail.com>
2021-02-05 09:56:39 +0300

    work on todo

--- dashboard-plugins/todo.js
+++ dashboard-plugins/todo.js
@@ -1,47 +1,72 @@
 module.exports = class TODOs {
-  
-    constructor(){
+  async initialize() {}
 
-      this.todogrid = new Muuri(".todogrid", {
-        itemClass: 'todo-item',
-        dragEnabled: true,
-      });
+  async render() {
+    return (
+      <div class="card mt-2 shadow bg-dark text-center">
+        <input id="todo-list" type="text" placeholder="Press Enter" />
+        <ul id="todolist" class="list-group">
 
-    }
-  
-    async render() {
-      return (
-        <div id="todogrid">
-        </div>
-      );
-    }
-  
-    async script() { 
-        let todos = localStorage.getItem("lastname");
+        </ul>
+      </div>
+    );
+  }
 
-        if (todos){
-            todos = JSON.parse(todos);
-            
-            todos.forEach(todo => this.addTodo(todo));
-            this.todogrid.refreshItems().layout();
-        }
-            
-    }
+  async script() {
+    this.renderTodos();
 
-    addTodo(){
+    document.getElementById("todo-list").addEventListener("keypress", this.addTodo.bind(this));
 
-      
-    }
+    document.getElementsByClassName("deletetodo").addEventListener("click", this.removeTodo.bind(this));
+  }
 
-    addTodo(todo){
+  addTodo(event) {
 
-        return this.todogrid.add(
-          <div class="todo-item">
-            <div class="item-content">
-              {todo}
-            </div>
-          </div>
-        );
+    if (event.key === "Enter") {
+      let todoInputElement = document.getElementById("todo-list");
+
+      let todos = JSON.parse(localStorage.getItem("todo-list")) || [];
+
+      if (todoInputElement.value) {
+        todos.push(todoInputElement.value);
+
+        console.log(JSON.stringify(todos));
+        localStorage.setItem("todo-list", JSON.stringify(todos));
+      }
+
+      todoInputElement.value = "";
     }
 
+    this.renderTodos();
+  }
+
+  renderTodos() {
+    let todos = localStorage.getItem("todo-list");
+
+    if (todos) {
+      todos = JSON.parse(todos);
+
+      document.getElementById("todolist").innerHTML = todos
+        .map((todo, index) => (
+          <li data-id={index} class="list-group-item todo d-flex justify-content-between align-items-center">
+            {todo}
+            <span class="deletetodo" class="badge badge-primary badge-pill">
+              X
+            </span>
+          </li>
+        ))
+        .join(" ");
+     
+      document.querySelectorAll('.todo ').forEach(element => {
+
+        element.addEventListener('click', this.removeTodo);
+      })
+        
+    }
+  }
+
+  removeTodo(event) {
+
+    console.log('x')
+  }
 };
