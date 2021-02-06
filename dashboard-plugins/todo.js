38549f07b82e105bd8bce76fd3e3baf5d5ecd272
YasserYka <yasseryka@gmail.com>
2021-02-06 16:18:36 +0300

    finished todo

--- dashboard-plugins/todo.js
+++ dashboard-plugins/todo.js
@@ -1,19 +1,17 @@
 module.exports = class TODOs {
   async initialize() {
-
     this.renderTodos = this.renderTodos.bind(this);
+    this.addTodo = this.addTodo.bind(this);
+    this.removeTodo = this.removeTodo.bind(this);
   }
 
   async render() {
     return (
       <div class="card mt-2 shadow bg-dark text-center">
         <div class="card-header">TODO</div>
-
         <div class="card-body">
-          <input id="todo-list" type="text" placeholder="Enter your task" />
-          <ul id="todolist" class="list-group mt-1">
-
-          </ul>
+          <input id="todo-input" type="text" placeholder="Enter your task" />
+          <ul id="todolist" class="list-group mt-1"></ul>
         </div>
       </div>
     );
@@ -22,63 +20,54 @@ module.exports = class TODOs {
   async script() {
     this.renderTodos();
 
-    document.getElementById("todo-list").addEventListener("keypress", this.addTodo.bind(this));
+    document.getElementById("todo-input").addEventListener("keypress", this.addTodo);
+  }
+
+  getTodos() {
+    return JSON.parse(localStorage.getItem("todo-list")) || [];
   }
 
   addTodo(event) {
-
     if (event.key === "Enter") {
-      let todoInputElement = document.getElementById("todo-list");
+      let input = document.getElementById("todo-input");
+      let todos = this.getTodos();
 
-      let todos = JSON.parse(localStorage.getItem("todo-list")) || [];
-
-      if (todoInputElement.value) {
-        todos.push(todoInputElement.value);
+      if (input.value) {
+        todos.push(input.value);
 
         localStorage.setItem("todo-list", JSON.stringify(todos));
       }
 
-      todoInputElement.value = "";
+      input.value = "";
     }
 
     this.renderTodos();
   }
 
   renderTodos() {
-    let todos = localStorage.getItem("todo-list");
+    let todos = this.getTodos();
 
-    if (todos) {
-      todos = JSON.parse(todos);
+    document.getElementById("todolist").innerHTML = todos
+      .map((todo, index) => (
+        <li data-id={index} class="list-group-item todo d-flex justify-content-between align-items-center">
+          {todo}
+          <span class="badge deletetodo badge-secondary badge-pill" style="cursor:pointer;">
+            X
+          </span>
+        </li>
+      ))
+      .join(" ");
 
-      document.getElementById("todolist").innerHTML = todos
-        .map((todo, index) => (
-          <li data-id={index} class="list-group-item todo d-flex justify-content-between align-items-center">
-            {todo}
-            <span class="badge deletetodo badge-secondary badge-pill" style="cursor:pointer;">
-              X
-            </span>
-          </li>
-        ))
-        .join(" ");
-     
-      document.querySelectorAll('.todo ').forEach(element => {
-
-        element.addEventListener('click', this.removeTodo.bind(this));
-      });
-        
-    }
+    document.querySelectorAll(".todo ").forEach((element) => {
+      element.addEventListener("click", this.removeTodo);
+    });
   }
 
   removeTodo(event) {
     let id = parseInt(event.currentTarget.dataset.id);
+    let todos = this.getTodos();
 
-    let todos = localStorage.getItem("todo-list");
-
-    if (todos) {
-      todos = JSON.parse(todos);
-
-      localStorage.setItem('todo-list' , JSON.stringify(todos.filter((_, index) => index !== id)));
-    }  
+    localStorage.setItem("todo-list", JSON.stringify(todos.filter((_, index) => index !== id)));
 
     this.renderTodos();
   }
