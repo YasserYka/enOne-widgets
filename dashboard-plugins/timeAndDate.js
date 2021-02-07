42271b3004af4e6a438d3f0f8f74683f9371394e
YasserYka <yasseryka@gmail.com>
2021-02-07 18:48:46 +0300

    greeting to timeanddate

---
+++ dashboard-plugins/timeAndDate.js
@@ -0,0 +1,31 @@
+// It's critical to declare imports with 'var'
+var got = require("got");
+
+module.exports = class TimeAndDate {
+  async initialize() {}
+
+  async render() {
+    return (
+      <div class="container text-center" style="color: white;">
+          <h1 id="clock"></h1>
+          <h4 id="day"></h4>
+          <h6 id="date"></h6>
+      </div>
+    );
+  }
+
+  async script() {
+    this.renderCurrentTime();
+
+    setInterval(this.renderCurrentTime, 60000 /* 1 minute in miliseconds */);
+  }
+
+  async renderCurrentTime() {
+    let date = new Date();
+
+    document.getElementById("clock").innerHTML = date.toLocaleTimeString([], { timeStyle: "short" });
+    document.getElementById("day").innerHTML = date.toLocaleString('en-us', {weekday:'long'});
+    document.getElementById("date").innerHTML = date.toLocaleDateString('en-us', {year: "numeric", month: "long", day: "numeric"})
+  }
+
+};
