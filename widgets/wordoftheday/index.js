e5d7e896803c0f81fe6409b0614b049ea2573c76
YasserYka <yasseryka@gmail.com>
2021-06-07 20:33:20 +0300

    two dep: got and g-i-s installation

--- widgets/wordoftheday/index.js
+++ widgets/wordoftheday/index.js
@@ -1,11 +1,40 @@
+var gis = require('g-i-s');
+const got = require("got");
+
+function logResults(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  else {
+    console.log(JSON.stringify(results, null, '  '));
+  }
+}
 
 module.exports = class WordOfTheDay {
 
-  async initialize(config) {}
+  async initialize(config) {
+
+  }
 
   async render() {
 
     return ();
+  }
+
+  async renderImage(error, images) {
+
+    if (error || images.length == 0)
+      console.error("couldn't fetch an image!");
+
+  }
+
+  // provider of random word API is https://github.com/mcnaveen/
+  async fetchWord() {
+
+    const url = "https://random-words-api.vercel.app/word";
+
+
+
   }
 
   async script() {}
