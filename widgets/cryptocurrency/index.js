0655769913addde5fb7b161be6f9c276beade34c
yasseryka <yasseryka@gmail.com>
2021-06-21 13:05:20 -0400

    fuck crypto icon source replaced all with default src

--- widgets/cryptocurrency/index.js
+++ widgets/cryptocurrency/index.js
@@ -6,6 +6,8 @@ module.exports = class Cryptocurrency {
   async initialize(config) {
 
     this.coins = config['coins'].join(',');
+    this.fetchData = this.fetchData.bind(this);
+    this.renderCoins = this.renderCoins.bind(this);
   }
 
   async render() {
@@ -18,25 +20,30 @@ module.exports = class Cryptocurrency {
   }
 
   async script() {
+
     this.renderCoins();
 
-    setInterval(this.renderRepositories, 10000 /* 10 seconds in miliseconds */);
+    setInterval(this.renderCoins, 10000 /* 10 seconds in miliseconds */);
   }
 
-  renderCoins() {
+  async renderCoins() {
+    const coinsListElement = document.getElementById("coins-list"); 
+
     this.fetchData().then((coins) => {
-  
-        coins.forEach((coin) => {
-            document.getElementById("coins-list").appendChild(
-              <div class="d-flex justify-content-between p-1">          
-                <div class="p-1" style="font-weight:bold;"><img class="p-1" style="width: 28px;" onerror={"this.src='" + __dirname + "/defaultcoin.png'"} src={"https://cryptoicons.org/api/icon/"+ coin.symbol.toLowerCase() +"/20"} /> {coin.name}</div>
-                <div class="p-1">
-                    <div style="font-size:16px">$ {parseFloat(coin.priceUsd).toFixed(3)}</div>
-                    <div class={"text-right badge" + coin.changePercent24Hr > 0 ? "text-success" : "text-danger"} style="font-size:12px">{parseFloat(coin.changePercent24Hr).toFixed(3) + "%"}</div>
-                </div>
+
+      coinsListElement.innerHTML = "";
+
+      coins.forEach((coin) => {
+          coinsListElement.appendChild(
+            <div class="d-flex justify-content-between p-1">          
+              <div class="p-1" style="font-weight:bold;"><img class="p-1" style="width: 28px;" src={__dirname + "/defaultcoin.png"} /> {coin.name}</div>
+              <div class="p-1">
+                  <div style="font-size:16px">$ {parseFloat(coin.priceUsd).toFixed(3)}</div>
+                  <div class={"text-right badge" + coin.changePercent24Hr > 0 ? "text-success" : "text-danger"} style="font-size:12px">{parseFloat(coin.changePercent24Hr).toFixed(3) + "%"}</div>
               </div>
-            );
-        });
+            </div>
+          );
+      });
     });
   }
 
