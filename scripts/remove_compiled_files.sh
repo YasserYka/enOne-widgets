761186a35496a9635e59660de0ecd1a0d889066e
yasseryka <yasseryka@gmail.com>
2021-06-24 14:14:49 -0400

    modified remove compiled files bash script

--- scripts/remove_compiled_files.sh
+++ scripts/remove_compiled_files.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-find . -name "compiled.js" -delete
+# when you modify index.js file of a widget manually you need to delete its compiled.js file
+# this script will help you to find and delete all compiled.js files 
+
+TARGET_FILE_NAMES="compiled.js"
+
+SOURCE_DIRECTORY=.
+
+echo -e "\n"
+
+find $SOURCE_DIRECTORY -name $TARGET_FILE_NAMES -print
+
+echo -e "\n"
+
+find $SOURCE_DIRECTORY -name $TARGET_FILE_NAMES -delete
+
+echo -e "These compiled widget files were removed successfully.\n"
